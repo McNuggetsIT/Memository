b7faf46af1a97a6766a1c06ad146969b28c29acd
annafirinu <anna.firinu2@outlook.it>
2025-11-18 16:38:24 +0100

    Sottoclasse FiltroMatrice
    Sottoclasse FiltroMatrice

---
+++ Esercizio di gruppo/moduli/filtromatrice.py
@@ -0,0 +1,16 @@
+from gestorematrice import GestoreMatrice
+
+class FiltroMatrice(GestoreMatrice):
+
+    def azzera_negativi(self):
+        for i in range(len(self.dati)): #Scorro gli indici delle righe
+            for j in range(len(self.dati[i])): #Scorro gli indici delle colonne
+                if self.dati[i][j] < 0: #Se il valore nella cella è negativo lo sostituisco con 0
+                    self.dati[i][j] = 0
+
+    def appiattisci(self):
+        lista_appiattita = [] #Creo una lista vuota che conterrà tutti i numeri
+        for riga in self.dati: #Scorro ogni riga
+            for valore in riga: #Scorro ogni numero della riga
+                lista_appiattita.append(valore) #Aggiungo ogni numero alla lista
+        return lista_appiattita #Restitisco la lista finale
